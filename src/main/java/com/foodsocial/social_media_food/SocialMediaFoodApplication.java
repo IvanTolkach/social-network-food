ccd7533a2d4ab7dc8cbf52beb6c6024269fd13e7
DeniaPolytech <murza.denis.andreevich@gmail.com>
2024-10-22 23:57:59 +0300

    Half-baked version of application's skeleton.

---
+++ src/main/java/com/foodsocial/social_media_food/SocialMediaFoodApplication.java
@@ -0,0 +1,13 @@
+package com.foodsocial.social_media_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodsocial/social_media_food/SocialMediaFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodsocial.social_media_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
