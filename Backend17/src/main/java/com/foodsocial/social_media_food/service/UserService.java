6a8a1621f7e63c2c07967742a684fefc7cb8ce92
Ivan Tolkach <ivantolkach2005@gmail.com>
2024-11-14 23:15:00 +0300

    Changed Java version to 17.

---
+++ Backend17/src/main/java/com/foodsocial/social_media_food/service/UserService.java
@@ -0,0 +1,11 @@
+package com.foodsocial.social_media_food.service;
+
+import com.foodsocial.social_media_food.accessingdatasql.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+    User registerUser(String username, String email, String password);
+
+    User loginUser(String identifier, String password);
+}
