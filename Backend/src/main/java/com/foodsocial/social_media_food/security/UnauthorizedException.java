158a5150624a47f6fed67c66ed1a71db787aa00f
Ivan Tolkach <ivantolkach2005@gmail.com>
2024-11-26 19:46:19 +0300

    deleted Backend folder

--- Backend/src/main/java/com/foodsocial/social_media_food/security/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.foodsocial.social_media_food.security;
-
-public class UnauthorizedException extends RuntimeException {
-  public UnauthorizedException(String message) {
-    super(message);
-  }
-}
--- Backend/src/test/java/com/foodsocial/social_media_food/SocialMediaFoodApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.foodsocial.social_media_food;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SocialMediaFoodApplicationTests {
-}
